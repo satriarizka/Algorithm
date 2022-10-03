99439f71b443bbf8e657880057cd456c288f6f08
Rizka Satria S XksatriaX <42266999+satriarizka@users.noreply.github.com>
2022-10-03 22:51:27 +0700

    Condition (if else)

---
+++ Kondisi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int nilai;
+	
+	cout<<"Input Nilai Mahasiswa: ";
+	cin>>nilai;
+	
+	if(nilai>75){
+		cout << "A";
+	}
+	else
+	cout << "B";
+	return 0;
+}
